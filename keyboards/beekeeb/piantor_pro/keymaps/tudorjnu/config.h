157c80133ae404ba69d163a5b5f96b59097569a6
Tudor Jianu <tudorjnu@gmail.com>
2024-08-06 11:08:52 +0100

    initial config

---
+++ keyboards/beekeeb/piantor_pro/keymaps/tudorjnu/config.h
@@ -0,0 +1,7 @@
+// mod-tap configuration
+#include "keymap_uk.h"
+
+#define TAPPING_TERM 200
+
+// CAPS WORD
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
---
+++ keyboards/beekeeb/piantor_pro/keymaps/tudorjnu/rules.mk
@@ -0,0 +1,5 @@
+CAPS_WORD_ENABLE = yes
+HOLD_ON_OTHER_KEY_PRESS = no
+
+# enable the keys for dynamic tapping term modification
+DYNAMIC_TAPPING_TERM_ENABLE = yes
