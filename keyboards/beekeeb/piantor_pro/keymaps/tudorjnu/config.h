acab68a67d6f9c6efccdb88c88710a1ded1f33e1
Tudor Jianu <jianu.tudor9@gmail.com>
2024-08-06 20:43:47 +0100

    add layer lock and autocorrect

--- keyboards/beekeeb/piantor_pro/keymaps/tudorjnu/config.h
+++ keyboards/beekeeb/piantor_pro/keymaps/tudorjnu/config.h
@@ -1,5 +1,8 @@
-// mod-tap configuration
-#define TAPPING_TERM 200
-
 // CAPS WORD
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+// mod-tap configuration
+#define TAPPING_TERM 180
+
+// Allows for compiling keycode
+#define ENABLE_COMPILE_KEYCODE
--- keyboards/beekeeb/piantor_pro/keymaps/tudorjnu/rules.mk
+++ keyboards/beekeeb/piantor_pro/keymaps/tudorjnu/rules.mk
@@ -1,8 +1,9 @@
 CAPS_WORD_ENABLE = yes
 HOLD_ON_OTHER_KEY_PRESS = no
-
+AUTOCORRECT_ENABLE = yes
 # enable the keys for dynamic tapping term modification
 DYNAMIC_TAPPING_TERM_ENABLE = yes
 
 # tap dance
 # TAP_DANCE_ENABLE = yes
+SRC += features/layer_lock.c
