14d1ae58c445a0074946d427bf24469afc42f508
Tudor Jianu <jianu.tudor9@gmail.com>
2025-04-28 08:47:17 +0100

    add debounce and fix gcc version

--- keyboards/beekeeb/piantor_pro/keymaps/tudorjnu/keymap.c
+++ keyboards/beekeeb/piantor_pro/keymaps/tudorjnu/keymap.c
@@ -17,7 +17,7 @@ enum layer_names {
 const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, LT(_SYM, KC_BSPC), KC_DEL);
 
 // This globally defines all key overrides to be used
-const key_override_t **key_overrides = (const key_override_t *[]){
+const key_override_t *key_overrides[] = {
     &delete_key_override,
     NULL // Null terminate the array of overrides!
 };
