6a45ac9181429abab5d42a4e6eab173ccffb64a5
Tudor Jianu <tjianu@liverpool.ac.uk>
2026-08-12 08:50:14 +0100

    merge upstream

--- keyboards/beekeeb/piantor_pro/keymaps/tudorjnu/config.h
+++ keyboards/beekeeb/piantor_pro/keymaps/tudorjnu/config.h
@@ -2,8 +2,12 @@
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 
 // mod-tap configuration
-#define TAPPING_TERM 220
+#define TAPPING_TERM 200
 // #define QUICK_TAP_TERM 100
 
 #define PERMISSIVE_HOLD
+// #define HOLD_ON_OTHER_KEY_PRESS
 #define DEBOUNCE 12
+
+// opposite hands rule
+// #define CHORDAL_HOLD
